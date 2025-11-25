735fba81a9a00a75b3c1b12c6fb888d8c16a6a48
amitxy <amitxy96@gmail.com>
2025-11-25 20:03:28 +0200

    Changed some os env

--- utils/helpers.py
+++ utils/helpers.py
@@ -11,12 +11,14 @@ CACHE_DIR = f'{SLURM_PATH}/tmp/hf_cache'
 
 def setup_slurm_env():
     if SLURM_PATH in os.getcwd():
+        os.makedirs(CACHE_DIR, exist_ok=True)
         os.environ["PIP_PATH"] = f"{SLURM_PATH}/BaryGNN/anaconda3/envs/conf/bin/pip"
         os.environ["TEMP_DIR"] = CACHE_DIR
         os.environ["HF_HOME"] = CACHE_DIR
-        # os.environ["TRANSFORMERS_CACHE"] = CACHE_DIR
-        os.environ["HF_DATASETS_CACHE"] = CACHE_DIR 
-        os.environ["HF_HUB_CACHE"] = CACHE_DIR
+        os.environ["TRANSFORMERS_CACHE"] = CACHE_DIR
+        os.environ["HF_DATASETS_CACHE"] = CACHE_DIR
+        # Hugging Face uses HUGGINGFACE_HUB_CACHE (HF_HUB_CACHE is ignored)
+        os.environ["HUGGINGFACE_HUB_CACHE"] = CACHE_DIR
         os.environ["TMPDIR"] = CACHE_DIR
         os.environ["XDG_CACHE_HOME"] = CACHE_DIR
         # os.environ["TOKENIZERS_PARALLELISM"] = "false"
