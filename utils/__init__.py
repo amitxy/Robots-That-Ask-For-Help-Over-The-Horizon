adbbcc6866b04c324e9a25810830f668f9c9efcf
amitxy <amitxy96@gmail.com>
2025-11-25 20:05:32 +0200

    Moved the order of imports

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,14 +1,14 @@
 """Utils package for Mind2Web experiments."""
 
 # Import submodules
-# from . import helpers
-from . import llm_utils as llm
-from . import plot_utils as plot
 
 # Expose commonly used items at package level
-from .helpers import (
+from utils.helpers import (
     CACHE_DIR,
     log_response,
     log_prompt,
     reload
 )
+
+from . import llm_utils as llm
+from . import plot_utils as plot
